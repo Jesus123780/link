366d5e6cd6a84cae809d7bc79d002298d298f541
Jesus123780 <53837840+Jesus123780@users.noreply.github.com>
2020-09-08 02:53:48 -0500

    Updates

---
+++ precache-manifest.30d6603b1da58b835eef2216b4307477.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99fd9f8a7de157c643b0ff3bec66dfc",
+    "url": "/link/index.html"
+  },
+  {
+    "revision": "c41124aeee9c4bb5791b",
+    "url": "/link/static/css/2.051905ae.chunk.css"
+  },
+  {
+    "revision": "592425bc48644d8872d7",
+    "url": "/link/static/css/main.f7c20bea.chunk.css"
+  },
+  {
+    "revision": "c41124aeee9c4bb5791b",
+    "url": "/link/static/js/2.e097757d.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/link/static/js/2.e097757d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "592425bc48644d8872d7",
+    "url": "/link/static/js/main.adba2b9d.chunk.js"
+  },
+  {
+    "revision": "bc2e9a27bfea9773f1ee",
+    "url": "/link/static/js/runtime-main.43f4c62c.js"
+  }
+]);
